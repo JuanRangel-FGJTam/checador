11808da526ef60da1b3809953ad2b7084cef1559
J. Salvador Rangel <juan.rangel@fgjtam.gob.mx>
2024-08-08 16:29:00 -0600

    👌 IMPROVE: use the Inertia link instead of the default anchor

--- resources/js/Components/Breadcrumb.vue
+++ resources/js/Components/Breadcrumb.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+import { Link } from '@inertiajs/vue3';
+
 const props = defineProps({
     breadcrumbs: {
         type: Array,
@@ -13,9 +15,10 @@ const props = defineProps({
     <nav class="flex" aria-label="Breadcrumb">
         <ol class="inline-flex items-center space-x-1 md:space-x-3">
             <li v-for="(crumb, index) in breadcrumbs" :key="index" class="inline-flex items-center">
-                <a  v-if="index !== breadcrumbs.length - 1" :href="crumb.href" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white" >
-                    <span>{{ crumb.name }}</span>
-                </a>
+
+                <Link v-if="index !== breadcrumbs.length - 1" :href="crumb.href" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                    <span>{{ crumb.name }}</span>                    
+                </Link>
                 <span v-else  class="text-sm font-medium text-gray-500 dark:text-gray-400" >
                     {{ crumb.name }}
                 </span>
