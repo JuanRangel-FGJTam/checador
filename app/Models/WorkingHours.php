b4d6463cc813bad7bd3aa36574d6311722b7776c
Juan Salvador Rangel Almaguer <juan.rangel@fgjtam.gob.mx>
2025-06-23 10:59:50 -0600

    Added a softDelete column to WorkingHours model

--- app/Models/WorkingHours.php
+++ app/Models/WorkingHours.php
@@ -3,17 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class WorkingHours extends Model
 {
+    use HasFactory, SoftDeletes;
+
     protected $fillable = [
         'employee_id',
         'checkin',
         'toeat',
         'toarrive',
-        'checkout'
+        'checkout',
+        'deleted_at'
     ];
-    
-    use HasFactory;
+
+
+    protected static function booted()
+    {
+        // * prevent having multiple workingHours active at the same time
+        static::creating(function ($workingHour)
+        {
+            // ensure delete previous working hours
+            WorkingHours::where('employee_id', $workingHour->employee_id)
+                ->whereNull('deleted_at')
+                ->delete();
+        });
+    }
+
 }
